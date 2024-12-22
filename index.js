5577a100bddfa60ae760fab88da6c8239d682f35
mininxd <mininsna@gmail.com>
2024-12-22 10:16:42 +0700

    add lokasi

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import axios from 'axios';
 const app = express();
 app.use(express.json());
 import { PrismaClient } from '@prisma/client'
@@ -9,9 +10,15 @@ app.get('/', (req, res) => {
 res.send({
 '/:nama_wilayah':'mendapatkan kode wilayah (Kecamatan & Desa)',
 '/adm2/:nama_wilayah':'mendapatkan kode wilayah (Provinsi & Kabupaten)',
+'/lokasi':'Mendapatkan info lokasi lat & long',
 '/kode/:kode_wilayah':'mendapatkan nama wilayah (Kecamatan & Desa)'
 	})
 })
+app.get('/lokasi', async (req, res) => {
+  res.send({
+    "/nama_wilayah":"pakailah nama desa atau kode adm4 untuk lebih akurat"
+  })
+});
 
 app.get('/:wilayah', async (req, res) => {
   try {
@@ -48,6 +55,32 @@ app.get('/adm2/:wilayah', async (req, res) => {
 
     if (wilayah && wilayah.length > 0) {
       res.send(wilayah[0].kode);
+    } else {
+      res.send("Data tidak ditemukan");
+    }
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+app.get('/lokasi/:wilayah', async (req, res) => {
+  try {
+    const wilayah = await prisma.wilayah.findMany({
+      where: { 
+        OR: [
+          {nama: req.params.wilayah},
+          {kode: req.params.wilayah},
+      ]
+      },
+      select: {
+        kode: true,
+        nama: true,
+      }
+    });
+
+    if (wilayah && wilayah.length > 0) {
+      let {data} = await axios.get(`https://api.bmkg.go.id/publik/prakiraan-cuaca?adm4=${wilayah[0].kode}`);
+      res.send(data.lokasi)
     } else {
       res.send("Data tidak ditemukan");
     }
